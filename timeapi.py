daf8df14ea484ad58dab429358d5372fd29d87a1
Hyperengined <71342876+Hyperengined@users.noreply.github.com>
2021-11-05 14:12:10 +0100

    Add the .py file

---
+++ timeapi.py
@@ -0,0 +1,29 @@
+#from tkinter import *
+import requests
+import json
+from pythonping import *
+
+target = "www.google.com"
+
+def jprint(obj):
+    # Create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+response = requests.get("http://worldclockapi.com/api/json/utc/now")
+
+
+print("===UTCtime========ping=======")
+while 0==0 : 
+    # get the ping
+    response_list = ping(target,count=10)
+    currentping = str(response_list.rtt_avg_ms)
+    #extract the time from the JSON
+    Time=response.json()["currentDateTime"][11:16]
+    #create the output
+    Time = "= " + Time + " = 10ping: " + currentping + " ms ="
+    print(Time, end = "\r")
+    #get the time
+    response = requests.get("http://worldclockapi.com/api/json/utc/now")
+
+
+
